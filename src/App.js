91bb2088d8feb6812c8d81da81e24fe8ae590ee1
Alexander Raschid <alexraschid1337@gmail.com>
2022-03-09 21:02:07 -0500

    added additoonal operators, passes tests 7/16

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import { Button, Container } from 'react-bootstrap';
+import { useState } from 'react';
 
 import { Col, Row, ListGroup } from 'react-bootstrap';
 export default App => {
 
+    let [displayText, setDisplayText] = useState(0);
+
+
     let generateNumPad = () => {
-        const DIGITS = ['1','2','3','4','5','6','7', '8', '9', '0', '.', 'x', '/', '+', '-']
-    
+        const DIGITS = ['0','1','2','3','4','5','6','7', '8', '9', '.', 'x', '/', '+', '-']
+        const DIGIT_NAMES = ['zero','one','two','three','four','five','six','seven','eight', 'nine'];
         return DIGITS.map((digit) => {
             console.log(digit);
                 for(let i = 0; i < DIGITS.length; i++){
-                    return (
-                            <Button>{digit}</Button>
-                        
-                    );
+                    switch(digit){
+                        case '.':
+                            return (<Button id={'decimal'}>{digit}</Button>);
+                        case 'x':
+                            return (<Button id={'multiply'}>{digit}</Button>);
+                        case '/':
+                            return (<Button id={'divide'}>{digit}</Button>);
+                        case '+':
+                            return (<Button id={'add'}>{digit}</Button>);
+                        case '-':
+                            return (<Button id={'subtract'}>{digit}</Button>);
+                        default:
+                            return (<Button id={DIGIT_NAMES[digit]}>{digit}</Button>);
+                    }  
                 }
-
         });
-        
     }
+
+
+
+
+    
 
     return (
         <Container fluid>
@@ -26,15 +43,18 @@ export default App => {
             </Row>
             <Row >
                 <Container fluid>
-                    <Row className="text-end">
+                <Row className="text-end">
                         <p>Result</p>
+                    </Row>
+                    <Row id='display' className="text-end">
+                        <p id='displayText'>{displayText}</p>
                     </Row>
                     <Row className="text-center">
                         <Col xs={6}>
-                         <Button>AC</Button>
+                         <Button id='clear'>AC</Button>
                         </Col>
                         <Col xs={6}>
-                         <Button>=</Button>
+                         <Button id="equals">=</Button>
                         </Col>
                     </Row>
                     <Row className="text-center">
